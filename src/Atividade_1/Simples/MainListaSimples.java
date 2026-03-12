7ac8c67b15e86d28ff40c6938905d445a93c7dd9
Erick <eshinfuku@gmail.com>
2026-03-12 19:11:48 -0300

    Atualizando o MainSimples

--- src/Atividade_1/Simples/MainListaSimples.java
+++ src/Atividade_1/Simples/MainListaSimples.java
@@ -4,25 +4,56 @@ public class MainListaSimples {
     static void main() {
         ListaSimples lista = new ListaSimples(5);
 
-        System.out.println("=== Adicionando Elementos [Tamanho 5] ===");
-        lista.adicionarElemento("Macarrao");
-        lista.adicionarElemento("Macarrao");
-        lista.adicionarElemento("Pao");
-        lista.adicionarElemento("Hamburguer");
-        lista.adicionarElemento("Feijao");
+        System.out.println("Adicionando elementos:");
+        lista.adicionarElemento("Gabriel");
+        lista.adicionarElemento("Erick");
+        lista.adicionarElemento("Alef");
+        lista.adicionarElemento("Erick");
 
-        System.out.println("=================================================");
+        System.out.println();
 
+        System.out.println("Contando os elementos:");
+        lista.contar();
         lista.exibirElementos();
+        System.out.println();
 
-        System.out.println("=================================================");
+        lista.removerTodas("Erick");
+        lista.exibirElementos();
+        lista.contar();
+        System.out.println();
 
-        // ContarOcorrencias
-        lista.contarOcorrencias("Macarrao");
+        String[] nomes = {"Ana", "Beatriz"};
 
-        System.out.println("=================================================");
+        lista.adicionarVarios(nomes);
+        lista.exibirElementos();
+        System.out.println();
 
-        // Substituir
-        lista.substituir("Macarrao", "Pera");
+        System.out.println("Obtendo o valor na posição desejada: ");
+        lista.obter(1);
+        System.out.println();
+
+        System.out.println("Inserindo o nome desejado: ");
+        lista.inserir(1, "Marcos");
+        lista.exibirElementos();
+        System.out.println();
+
+        System.out.println("Removendo o valor pelo índice");
+        lista.removerPorIndice(1);
+        lista.exibirElementos();
+        System.out.println();
+
+//        Limpando a lista
+//        lista.limpar();
+//        lista.exibirElementos();
+
+        lista.ultimoIndiceDe("Ana");
+        System.out.println();
+
+        lista.contarOcorrencias("Ana");
+        System.out.println();
+
+        System.out.println("Substituindo os elementos");
+        lista.substituir("Ana", "Erick");
+        lista.exibirElementos();
     }
 }
